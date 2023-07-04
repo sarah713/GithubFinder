0eb4ef745892151ee9a120dd3b091cc76f995b4a
Sarah <sarahkulsoom18@gmail.com>
2023-07-04 20:08:24 +0530

    Added all files

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import spinner from "./assets/spinner.gif";
+function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+      <img
+        width={120}
+        src={spinner}
+        alt="Loading.."
+        className="text-center mx-auto"
+      />
+    </div>
+  );
+}
+
+export default Spinner;
